5426dc3cdaa7a19e7a55d55e6560c4a5e96ef090
Xshark071 <omar.alissa2756@gmail.com>
2025-01-08 14:57:41 +0100

    carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cibando';
+  percorso= "../assets/images/carousel-";
+  images = [
+    {id : 1, label: "spaghetti al pomodoro"},
+    {id : 2, label: "tagliata di manzo"},
+    {id : 3, label: "tiramisù"}
+  ]
 }
